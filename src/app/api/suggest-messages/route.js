13ffae0728766ad1b30f04675af2053812728e9f
ganzander <ganeshmangla2003@gmail.com>
2024-06-26 18:02:20 +0530

    Gemini Connected

--- src/app/api/suggest-messages/route.js
+++ src/app/api/suggest-messages/route.js
@@ -1,20 +1,20 @@
-import OpenAI from "openai";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const API_KEY = process.env.GEMINI_API_KEY;
 
 export async function GET(req) {
-  const openai = new OpenAI(process.env.OPENAI_API_KEY);
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
   const messagePrompt =
     "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
   try {
-    const response = await openai.createCompletion({
-      engine: "gpt-3.5-turbo-instruct",
-      max_tokens: 400,
-      prompt: messagePrompt,
-    });
-
-    return Response.json({ data: "hello" }, { status: 200 });
+    const result = await model.generateContent(messagePrompt);
+    const chatResult = result.response.text();
+    return Response.json({ data: chatResult }, { status: 200 });
   } catch (error) {
     console.log(error);
-    return Response.json({ data: error }, { status: 400 });
+    return Response.json({ data: error.message }, { status: 400 });
   }
 }
