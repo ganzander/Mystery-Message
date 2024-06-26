3a6e052b34d93c84491f174051846ba065a78436
ganzander <ganeshmangla2003@gmail.com>
2024-06-26 17:26:50 +0530

    Updated Key

--- src/app/api/suggest-messages/route.js
+++ src/app/api/suggest-messages/route.js
@@ -1,9 +1,7 @@
 import OpenAI from "openai";
 
 export async function GET(req) {
-  const openai = new OpenAI(
-    "sk-proj-ZPia8fxjMrHFPTndHEsgT3BlbkFJpeaYjIcDgveSFb2UnRM6"
-  );
+  const openai = new OpenAI(process.env.OPENAI_API_KEY);
   const messagePrompt =
     "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
